77f3fceb51f6daab41c6e6f70527e660f98eb035
cave3 <wrks@mac.com>
2017-02-24 14:47:16 +0100

    added html write out

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 // Basic web-server
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '127.0.0.1'
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200; // 200 = everything ok
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Server is up ..');
-});
+fs.readFile('index.html', (err, html) => {
+  if(err) {
+    throw err;
+  }
 
-// @params: port
-// @params: hostname
-// @params: callbacl function
-server.listen(port, hostname, () => {
-  console.log(`Server running at port http://${hostname}:${port}/`);
+  const server = http.createServer((req, res) => {
+    res.statusCode = 200; // 200 = everything ok
+    res.setHeader('Content-Type', 'text/html');
+    res.write(html);
+    res.end();
+  });
+
+  // @params: port
+  // @params: hostname
+  // @params: callbacl function
+  server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
 });
